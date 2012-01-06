48f30b483906ce93a736d9b9afe96cc10cf445c5
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2012-01-06 17:59:04 +0000

    Have CPAN::Access::AdHoc::Default::CPAN::CPAN::Mini convert the local repository location to a POSIX file spec before slapping 'file://' on the front.
    git-svn-id: file:///Users/tom/Code/svn/repos/CPAN-Access-AdHoc/trunk@2895 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/CPAN/Access/AdHoc/Default/CPAN/CPAN/Mini.pm
+++ lib/CPAN/Access/AdHoc/Default/CPAN/CPAN/Mini.pm
@@ -7,6 +7,8 @@ use warnings;
 
 use CPAN::Access::AdHoc::Util;
 use Cwd ();
+use File::Spec;
+use File::Spec::Unix;
 
 our $VERSION = '0.000_01';
 
@@ -28,7 +30,11 @@ sub get_default {
     $local = Cwd::abs_path( $local );
     -d $local
 	or return;
-    return 'file://' . $local;
+    my ( $dev, $dir, $base ) = File::Spec->splitpath( $local );
+    defined $dev
+	and $dev =~ s/ : \z /|/smx;	# VMS, Windows
+    my @path = grep { defined $_ && $_ ne '' } File::Spec->splitdir( $dir );
+    return 'file://' .  File::Spec::Unix->catfile( $dev, @path, $base );
 }
 
 1;
