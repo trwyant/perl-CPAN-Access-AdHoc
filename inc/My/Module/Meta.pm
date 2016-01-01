51f1c749cb2bdf923bd0c2b6516c98ad294117eb
Tom Wyant <wyant@cpan.org>
2016-01-01 16:40:35 -0500

    Move merge_meta info to My::Module::Meta.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -35,6 +35,26 @@ sub distribution {
     return $self->{distribution};
 }
 
+sub meta_merge {
+    return {
+	'meta-spec'	=> {
+	    version	=> 2,
+	},
+	no_index	=> {
+	    directory	=> [ qw{ inc t xt } ],
+	},
+	resources	=> {
+#	    bugtracker	=> 'https://rt.cpan.org/Public/Dist/Display.html?Name=CPAN-Access-AdHoc',
+	    license	=> 'http://dev.perl.org/licenses/',
+#	    repository	=> {
+#		type	=> 'git',
+#		url	=> 'git://github.com/trwyant/perl-CPAN-Access-AdHoc.git',
+#		web	=> 'https://github.com/trwyant/perl-CPAN-Access-AdHoc',
+#	    },
+	}
+    };
+}
+
 sub requires {
     my ( $self, @extra ) = @_;
 ##  if ( ! $self->distribution() ) {
@@ -132,6 +152,16 @@ C<BUILD_REQUIRES> key.
 
 This method returns the value of the environment variable
 C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
+
+=head2 meta_merge
+
+ use YAML;
+ print Dump( $meta->meta_merge() );
+
+This method returns a reference to a hash describing the meta-data which
+has to be provided by making use of the builder's C<meta_merge>
+functionality. This includes the C<dynamic_config>, C<no_index> and
+C<resources> data.
 
 =head2 requires
 
